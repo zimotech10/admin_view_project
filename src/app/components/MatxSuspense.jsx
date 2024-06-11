8fc8155aa4a80b10af3438f762ecdbe2e1110872
David Duncan <seadragon1997111@gmail.com>
2024-06-11 12:37:28 +0200

    Optimize file upload handling

--- src/app/components/MatxSuspense.jsx
+++ src/app/components/MatxSuspense.jsx
@@ -5,4 +5,4 @@ export default function MatxSuspense({ children }) {
   return <Suspense fallback={<MatxLoading />}>{children}</Suspense>;
 }
 
-//asdf dfsdsdfsdf dsfd sdfsd sdfsd
+//asdf dfsdsdfsdf dsfd sdfsd sdfsd sdfsd sds
