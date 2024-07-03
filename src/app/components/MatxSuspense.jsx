f7558c85924e7049de25ce53beeeaaf23423e70f
David Duncan <seadragon1997111@gmail.com>
2024-07-03 09:55:50 +0200

    Implement feature toggles for A/B testing

--- src/app/components/MatxSuspense.jsx
+++ src/app/components/MatxSuspense.jsx
@@ -5,4 +5,4 @@ export default function MatxSuspense({ children }) {
   return <Suspense fallback={<MatxLoading />}>{children}</Suspense>;
 }
 
-//asdf dfsdsdfsdf dsfd sdfsd sdfsd sdfsd sds sdfsd
+//asdf dfsdsdfsdf dsfd sdfsd sdfsd sdfsd sds sdfsd sdfasdf
